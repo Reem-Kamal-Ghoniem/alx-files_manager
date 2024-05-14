753fb758862fac6df4b2cd645c44f96a8d65d7bf
Reem Kamal Ghoniem <reemkamal111@gmail.com>
2024-05-15 00:11:14 +0200

    task1.1

---
+++ utils/redis.js
@@ -0,0 +1,58 @@
+const redis = require('redis');
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+
+        this.client.on('error', (err) => {
+            console.error('Redis client not connected to the server:', err);
+        });
+
+        this.client.on('connect', () => {
+            console.log('Redis client connected to the server');
+        });
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, reply) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+
+    async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+            this.client.setex(key, duration, value, (err, reply) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err, reply) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
