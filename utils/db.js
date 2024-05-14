02ca9cdbcaaa181a53e3a51da28798a11a9a82c1
Reem Kamal Ghoniem <reemkamal111@gmail.com>
2024-05-15 00:56:49 +0200

    task2.3

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,12 @@
 const { MongoClient } = require('mongodb');
 
-class DBClient {
-    constructor() {
         const host = process.env.DB_HOST || 'localhost';
         const port = process.env.DB_PORT || 27017;
         const database = process.env.DB_DATABASE || 'files_manager';
         const url = `mongodb://${host}:${port}`;
+
+class DBClient {
+    constructor() {
 
         MongoClient.connect(url, (err, client) => {
             if (!err) {
