74a86ccb45238a825a7d32033199b37ca47ec24c
Reem Kamal Ghoniem <reemkamal111@gmail.com>
2024-05-15 00:51:18 +0200

    task2.2

--- utils/db.js
+++ utils/db.js
@@ -5,27 +5,26 @@ class DBClient {
         const host = process.env.DB_HOST || 'localhost';
         const port = process.env.DB_PORT || 27017;
         const database = process.env.DB_DATABASE || 'files_manager';
+        const url = `mongodb://${host}:${port}`;
 
-        const uri = `mongodb://${host}:${port}/${database}`;
-
-        this.client = new MongoClient(uri);
-    }
+        MongoClient.connect(url, (err, client) => {
+            if (!err) {
+              this.db = client.db(database);
+            } else {
+              this.db = false;
+            }
+          });
+        }
     isAlive() {
         return this.isConnected;
     }
 
     async nbUsers() {
-        if (!this.isConnected) return -1;
-
-        const usersCollection = this.client.db().collection('users');
-        return usersCollection.countDocuments();
+        return this.db.collection('users').countDocuments();
     }
 
     async nbFiles() {
-        if (!this.isConnected) return -1;
-
-        const filesCollection = this.client.db().collection('files');
-        return filesCollection.countDocuments();
+        return this.db.collection('files').countDocuments();
     }
 }
 
