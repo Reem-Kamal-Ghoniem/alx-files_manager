0314acacb9bc2d39cb149f935c455455a0ee60f5
reem <tarekreem950@gmail.com>
2024-05-16 02:18:24 +0300

    userController

---
+++ controllers/UsersController.js
@@ -0,0 +1,9 @@
+export const postNew = async(req, res) => {
+    const {email, password} = req.body
+    if (!email){
+        return res.status(400).json({error: "Missing email"})
+    }
+    if (!password){
+        return res.status(400).json({error: "Missing password"})
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { getStatus, getStats } from '../controllers/AppController';
+import { postNew } from '../controllers/UsersController'
 
 const route = express.Router();
 
 route.get('/status', getStatus);
 route.get('/stats', getStats);
+route.post('users', postNew)
 
 export default route;
